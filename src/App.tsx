5a033ac0a050315c52b3c544e1d694d509989732
森下勇樹 <>
2024-08-24 19:07:01 +0900

    delete unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import './App.css';
-import { Button, ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter, Link } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom";
 // ローカルインポート
 import theme from './theme/theme';
-import { Router } from "./router/Router"
+import { Router } from "./router/Router";
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
         <BrowserRouter>
-          {/* <Link to="/">Home</Link><br />
-          <Link to="/login">Login</Link><br />
-          <Link to="/setting">Setting</Link><br />
-          <Link to="/user-mng">UserManagement</Link><br /> */}
-
           <Router />
         </BrowserRouter>
       </ChakraProvider>
