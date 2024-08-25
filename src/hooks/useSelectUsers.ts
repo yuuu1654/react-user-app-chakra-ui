d28481a7129d6788643761ccf929c570d83eb50d
森下勇樹 <>
2024-08-26 06:44:50 +0900

    delete unnecessary file

--- src/hooks/useSelectUsers.ts
+++ src/hooks/useSelectUsers.ts
@@ -13,15 +13,13 @@ interface Props {
  */
 export const useSelectUsers = () => {
   const [selectedUser, setSelectedUser] = useState<User | null>(null)
+  
   const findUser = useCallback((props: Props) => {
     const { id, users, onOpen } = props;
     const targetUser = users.find((user) => user.id === id)
     // ▼ 今回、findが値を返さない事はないので、undefinedの可能性を明示的に排除する (あまり使わない方がいい)
     // setSelectedUser(targetUser ?? null) <= targetUserがundefinedならnullを入れて型エラーを回避
-    console.log(id) // idは、きちんと取れてきている
-    console.log("targetUser", targetUser)
     setSelectedUser(targetUser!)
-    console.log("selectedUser", selectedUser)
     onOpen()
   }, [])
   return { findUser, selectedUser }
